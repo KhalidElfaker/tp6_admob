13cccccd2c89b8e9f618bb432a7c94e7acc112ef
khalid elfaker <khalidelfaker@gmail.com>
2020-05-02 13:47:10 +0000

    initial commit 2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig, AdMobFreeRewardVideoConfig } from '@ionic-native/admob-free/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,59 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private admobFree: AdMobFree) {}
+
+  showPub(){
+    const bannerConfig: AdMobFreeBannerConfig = {
+      // add your config here
+      // for the sake of this example we will just use the test config
+      isTesting:true,
+      autoShow: true,
+
+     };
+     this.admobFree.banner.config(bannerConfig);
+     
+     this.admobFree.banner.prepare()
+       .then(() => {
+         // banner Ad is ready
+         // if we set autoShow to false, then we will need to call the show method here
+       })
+       .catch(e => console.log(e));
+  }
+
+  showRewardVideo(){
+    const rewardVideoConfig: AdMobFreeRewardVideoConfig = {
+      // add your config here
+      // for the sake of this example we will just use the test config
+      isTesting:true,
+      autoShow: true,
+
+     };
+     this.admobFree.rewardVideo.config(rewardVideoConfig);
+     
+     this.admobFree.rewardVideo.prepare()
+       .then(() => {
+         // banner Ad is ready
+         // if we set autoShow to false, then we will need to call the show method here
+       })
+       .catch(e => console.log(e));
+  }
+  showInterstitial(){
+    const interstitialConfig: AdMobFreeInterstitialConfig = {
+      // add your config here
+      // for the sake of this example we will just use the test config
+      isTesting:true,  
+      autoShow: true,
+
+     };
+     this.admobFree.interstitial.config(interstitialConfig);
+     
+     this.admobFree.interstitial.prepare()
+       .then(() => {
+         // banner Ad is ready
+         // if we set autoShow to false, then we will need to call the show method here
+       })
+       .catch(e => console.log(e));
+  }
 
 }
